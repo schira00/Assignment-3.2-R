a16d2b06b0801aa9f5a28122a736df5501e1e770
schira00 <157739897+schira00@users.noreply.github.com>
2024-01-26 15:46:07 +0100

    Update Assignment 3.2.R.R

--- Assignment 3.2.R.R
+++ Assignment 3.2.R.R
@@ -6,7 +6,7 @@
 # Function remind me: 
 
 remind_me <- function(){
-  reminder <- "Remember to buy grocieries: Eggs, Milk, Chicken, Bannas"
+  reminder <- "Remember to buy groceries: Eggs, Milk, Chicken, Bannas"
   return(reminder)
 }
 
@@ -30,7 +30,7 @@ cheat <- function(exercise){
     result <- "(chick_weight <- ChickWeight 
                 
                 ChickWeight$Chick 
-                \
+                
                 max_weight <- aggregate(x = weight ~ Chick, 
                                         FUN = max, 
                                         data = ChickWeight) 
@@ -49,20 +49,53 @@ cheat <- function(exercise){
 
 cheat("exercise 2")
 
+# Link:
+# https://github.com/schira00/Assignment-3.2-R/blob/main/Assignment%203.2.R.R
+
 
 # Question 3.2 R.2
 
 install.packages("DescTools")
 library(DescTools)
 
-dev.new(width=12, height=4)
-Canvas(xlim = c(0,50), ylim=c(2, 6), asp=1, xpd=TRUE)
-DrawCircle(x=plotdat$xcords, y=plotdat$ycords, r.out = 2)
+install.packages("plotrix")
+library("plotrix")
 
-
-make_art <- function(){
+make_art <- function(x){
+  set.seed(x)
+  plot(1, type = "n", xlab = "", ylab = "", xlim = c(0, 10), ylim = c(0, 10))
+  
+  for(i in 1:20){
+    radius <- runif(1, 0.3, 1.5)
+    a <- runif(1, 0, 9)
+    b <- runif(1, 0, 9)
+    color <- rgb(runif(1), runif(1), runif(1))
+    symbols(a, b, circles = radius, inches = 0.1, add = TRUE, bg = color, fg = NULL)
+  }
+  
+  for(i in 1:10){
+    a1 <- runif(1, 0, 7)
+    a2 <- runif(1, 0, 7)
+    b1 <- runif(1, 0, 10)
+    b2 <- runif(1, 0, 10)
+    col <- rgb(runif(1), runif(1), runif(1))
+    lines(c(a1, a2), c(b1, b2), col = col, lwd = 2)
+    
+  }
+  
+  for(i in 1:5){
+    xleft <- runif(1, 4, 7)
+    ybottom <- runif(1,6, 7)
+    xright <- runif(1, 3, 7)
+    ytop <- runif(1, 5, 7)
+    col <- rgb(runif(1), runif(1), runif(1))
+    rect(xleft, ybottom, xright, ytop, density = NULL, angle = 45,
+         col = col, border = NULL, lty = par("lty"), lwd = par("lwd"))
+  }
   
 }
 
+make_art(123)
 
+# https://github.com/schira00/Assignment-3.2-R/blob/main/README.md
 
